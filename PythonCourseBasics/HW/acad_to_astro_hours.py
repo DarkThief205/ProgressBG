7481eaed5a0ec5185a16373dcc504033d3e57aa1
DarkThief205 <darkthief@abv.bg>
2025-07-01 00:51:00 +0300

    added 2 files to HW

---
+++ PythonCourseBasics/HW/acad_to_astro_hours.py
@@ -0,0 +1,15 @@
+
+academical_hours = 64
+acad_hour_minutes = 40
+session_size = 4
+break_minutes = 20
+astro_hour_minutes = 60
+
+sessions = academical_hours // session_size
+
+total_minutes = academical_hours * acad_hour_minutes + sessions * break_minutes
+
+
+total_astro_hours = total_minutes / astro_hour_minutes
+
+print(f"Total duration in astronomical hours: {total_astro_hours:.2f}")
---
+++ PythonCourseBasics/HW/body_mass_index_calculator.py
@@ -0,0 +1,6 @@
+weight_in_kilogram = float(input("Enter your weight: "))
+height_in_meters = float(input("Enter your height: "))
+
+BMI = weight_in_kilogram / (height_in_meters**2)
+print(f"Your BMI is: {BMI:.2f}")
+
