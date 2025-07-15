b135aca43087c8c42d7b3d93048c48b5f6128e2d
DarkThief205 <darkthief@abv.bg>
2025-07-16 02:15:04 +0300

    added function_task1 --> Variant 1 (hard coded) (No errors)

---
+++ PythonCourseBasics/HW/Tasks Functions/tasks_functions1.py
@@ -0,0 +1,19 @@
+# ---------------------------------- Task 1 ---------------------------------- #
+"""DESCRIPTION:
+Write a function named 'calculate_area' that takes two parameters (length and width)
+and returns the area of a rectangle.
+"""
+
+
+### YOUR CODE HERE
+def calculate_area(lenght, width):
+    area_calcuation = lenght * width
+    print(area_calcuation)
+calculate_area(10,20)
+### TEST:
+# print(calculate_area(10, 5))
+
+### EXPECTED OUTPUT:
+# 50
+
+# ---------------------------------------------------------------------------- #
