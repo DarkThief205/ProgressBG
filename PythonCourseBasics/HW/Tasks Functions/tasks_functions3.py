c53b73db68431d7132f8da6581d0a09435433d3e
DarkThief205 <darkthief@abv.bg>
2025-07-16 02:55:46 +0300

    added task_functions3.py working (No errors)

---
+++ PythonCourseBasics/HW/Tasks Functions/tasks_functions3.py
@@ -0,0 +1,18 @@
+# ---------------------------------- Task 3 ---------------------------------- #
+""" DESCRIPTION:
+Write a function named 'multiply_elements' that takes a list of integers and returns the product of all the elements in the list.
+"""
+
+
+### YOUR CODE HERE
+def multiply_elements(a,b,c):
+    return a * b * c
+
+print(multiply_elements(a = 2,b = 3, c = 4))
+
+### TEST:
+# print(multiply_elements([2, 3, 4]))
+
+### EXPECTED OUTPUT:
+# 24
+# ---------------------------------------------------------------------------- #
