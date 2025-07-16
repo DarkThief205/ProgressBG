0f2273756d5d19a3cb8d42aeda170bcd3de45da0
DarkThief205 <darkthief@abv.bg>
2025-07-16 03:06:39 +0300

    created tasks_functions5.py (Pass)

---
+++ PythonCourseBasics/HW/Tasks Functions/tasks_functions5.py
@@ -0,0 +1,17 @@
+# ---------------------------------- Task 5 ---------------------------------- #
+""" DESCRIPTION:
+Write a lambda function 'reverse_string' that takes a string and returns the string reversed.
+"""
+
+### YOUR CODE HERE
+def reverse_string(string):
+    pass
+
+
+print(reverse_string("hello"))
+### TEST:
+# print(reverse_string("hello"))
+
+### EXPECTED OUTPUT:
+# "olleh"
+# ---------------------------------------------------------------------------- #
