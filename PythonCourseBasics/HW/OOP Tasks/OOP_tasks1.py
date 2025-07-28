e4fed81f3b449b94734038748883e7717c4be88f
DarkThief205 <darkthief@abv.bg>
2025-07-28 12:07:28 +0300

    "return" does not return str.

---
+++ PythonCourseBasics/HW/OOP Tasks/OOP_tasks1.py
@@ -0,0 +1,40 @@
+# ---------------------------------- Task 1 ---------------------------------- #
+"""DESCRIPTION:
+Create a class called 'Car' to represent a car. The class should have the following attributes and methods:
+
+Attributes:
+- 'make': a string representing the make of the car (e.g., Toyota, Honda).
+- 'model': a string representing the model of the car (e.g., Camry, Civic).
+- 'year': an integer representing the manufacturing year of the car.
+
+Methods:
+- 'start_engine()': a method that prints a message indicating that the car's engine has started.
+- 'stop_engine()': a method that prints a message indicating that the car's engine has stopped.
+
+Create an instance of the 'Car' class with information about a car and demonstrate the usage of the 'start_engine' and 'stop_engine' methods.
+
+"""
+
+
+### Define the Car class
+class Car:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+
+    def start_engine(self):
+        return f"Engine started for {self.make} {self.model} {self.year}"
+    
+    def stop_engine(self):
+        return f"Engine stopped for {self.make} {self.model} {self.year}"
+    
+    
+### TEST
+car1 = Car("Toyota", "Camry", 2022)
+car1.start_engine()
+car1.stop_engine()
+
+### EXPECTED OUTPUT:
+# Engine started for Toyota Camry 2022
+# Engine stopped for Toyota Camry 2022
